610764d8daf8e11ba4313baeca85793f3df3cc6f
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:25 +0330

    Create display-a-checkerboard-pattern-of-stars-and-plus-signs.c

---
+++ display-a-checkerboard-pattern-of-stars-and-plus-signs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main (){
+int n,i,j;
+printf("\nEnter n: ");
+scanf ("%d",&n);
+for (i=1;i <= n;i++){
+    for (j=1; j <= i; j++)
+    if (j%2 == 0)
+    printf("+");
+    else
+    printf("*");
+    printf("\n");
+}
+getch ();
+return(0);
+}
