a0aba2f2a23c627063dc434d267c87657c759b7f
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:17 +0330

    Create day-and-week-calculation.c

---
+++ day-and-week-calculation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    int n, week, day;
+    printf("\nEnter n:");
+    scanf("%d",&n);
+    week=(n-1)/7+1;
+    day= (n-1)%7+1;
+    printf("day = %d\tweek=%d",day,week);
+    getch ();
+    return(0);
+}
