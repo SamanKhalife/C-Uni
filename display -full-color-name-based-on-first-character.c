0518ac3da9d1f0e82ac3f98d4ea1924742fba8be
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:22 +0330

    Create display -full-color-name-based-on-first-character.c

---
+++ display -full-color-name-based-on-first-character.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+char ch;
+switch (ch=getchar ())
+{
+case 'b':
+case 'B': printf("Blue");
+    break;
+case 'r':
+case 'R': printf("Red");
+    break;
+case 'g':
+case 'G': printf("Green");
+    break;
+default: printf("Error!");
+}
+getch();
+return(0);
+}
