42f979ddda1c220458798faae72a4af4c11edfa8
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:39 +0330

    Create display-a-pattern-of-repeated-numbers-decreasing.c

---
+++ display-a-pattern-of-repeated-numbers-decreasing.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+int n,i,j;
+printf("\nEnter n: ");
+scanf("%d", &n);
+for (i=1;i <= n;i++){
+    for (j=1;j <= i;j++)
+    printf("%3d",n-i+1);
+    printf("\n");
+ }
+getch();
+return (0);
+}
