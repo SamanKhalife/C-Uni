9a132338c355a791c3e801554a7c03412bc4dad5
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:05:03 +0330

    Create display-fibonacci-sequence.c

---
+++ display-fibonacci-sequence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int i,a,b,c;
+a=b=1;
+printf("\n%4d%4d",a,b);
+for (i=3;i <= 15;i++){
+    c=a+b;
+    printf("%4d",c);
+    a=b;
+    b=c;
+}
+getch ();
+return (0);
+}
