abe666b8635d11c5cadfd228d82b46b882a6d7ef
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:31 +0330

    Create display-a-hollow-square-with-stars.c

---
+++ display-a-hollow-square-with-stars.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main (){
+int n,i,j;
+printf("\nEnter n :");
+scanf("%d",&n);
+if ((n<1) || (n>20))
+printf("Error! input is too big!");
+else{
+    for (i=1;i <= n;i++)
+      printf("*");
+    printf("\n");
+    for (i=1;i <= n-2;i++){
+      printf("*");
+    for (j=1;j <= n-2; j++)
+      printf(" ");
+    printf("*\n");
+    }
+    for (i=1;i <= n;i++)
+    printf("*");
+    }
+getch ();
+return (0);
+}
