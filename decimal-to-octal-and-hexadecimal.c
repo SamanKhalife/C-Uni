aa96e8fa19e03f4906715227edc79c27e87a1b29
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:19 +0330

    Create decimal-to-octal-and-hexadecimal.c

---
+++ decimal-to-octal-and-hexadecimal.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+int x;
+printf("\n Enter x:");
+scanf("%d",&x);
+printf("%d base 10 equal to %o base 8\n",x,x);
+printf("d base 10 equal to %x base 16\n",x,x);
+getch();
+return(0);
+}
