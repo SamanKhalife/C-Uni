06bcc03ad98175097a765d080c2f4dd9599320a8
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:05:22 +0330

    Create variable-exchange.c

---
+++ variable-exchange.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    printf("\nEnter a:");
+    scanf("%d", &a);
+    printf("\nEnter b:");
+    scanf("%d", &b);
+
+    c=a;
+    a=b;
+    b=c;
+
+    printf("a = %d \n b = %d \n",a,b);
+    getchar();
+    return 0;
+}
