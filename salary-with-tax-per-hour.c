664d400b8e671a4f48021e0f670f6a5a6e86058d
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:05:19 +0330

    Create salary-with-tax-per-hour.c

---
+++ salary-with-tax-per-hour.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float d,h,w,m;
+    printf("\nEnter Houres and base:");
+    scanf("%f%f",&h, &d);
+    w=h*d;
+    m=(w<16000)?0:0.1*(w-16000);
+    printf("payment =%7.1f \n ",w-m);
+    getchar();
+    return 0;
+}
