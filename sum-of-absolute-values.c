5ec7643e54035a1ef1741529fea24633a4fc9f46
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:05:25 +0330

    Create sum-of-absolute-values.c

---
+++ sum-of-absolute-values.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+int x,y, z;
+printf("\n Enter x,y:");
+scanf ("%d%d", &x, &y) ;
+z=x>=0?(y>=0)?x+y:x-y:(y>=0)?y-x :- y-x;
+printf(" |%d| +|%d| =%d ",x,y, z) ;
+getch ();
+return (0);
+}
