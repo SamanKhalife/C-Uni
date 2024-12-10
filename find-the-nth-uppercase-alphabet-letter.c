2dcb489c7d7765a5eac979fc2b8fcee7d5c4645b
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:53 +0330

    Create find-the-nth-uppercase-alphabet-letter.c

---
+++ find-the-nth-uppercase-alphabet-letter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+int n;
+char ch1,ch2;
+printf("\n Enter n :");
+scanf("%d",&n);
+ch1='A'+n-1;
+printf(" %dth char of alphabet is %c ",n,ch1);
+getch();
+return(0);
+}
