e8b0f6089f764ee40fbf5bb3b3afbbb510f2d41a
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:10 +0330

    Create celcius-to-farenhayt.c

---
+++ celcius-to-farenhayt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int c;
+    float f;
+    printf("\nTemprature ('c):");
+    scanf("%d", &c);
+
+    f=c*1.8 + 32;
+
+    printf("%d 'c = %-5.1f Farenhayt\n", c, f);
+    getchar();
+    return 0;
+}
