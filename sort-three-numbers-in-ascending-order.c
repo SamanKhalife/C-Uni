69986decfbf0fb831f29767696ff2e82d98d3440
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:05:07 +0330

    Create sort-three-numbers-in-ascending-order.c

---
+++ sort-three-numbers-in-ascending-order.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main (){
+float a,b,c,t;
+printf("\nEnter a,b,c: ");
+scanf("%f%f%f",&a, &b,&c);
+  if (a>b) {
+    t=b;
+    b=a;
+    a=t;
+  }
+  if (a>c) {
+    t=c;
+    c=a;
+    a=t;
+  }
+  if (b>c) {
+    t=c;
+    c=b;
+    b=t;
+  }
+  Printf("%d %d %d",a, b, c) ;
+  getch ();
+  return(0);
+}
