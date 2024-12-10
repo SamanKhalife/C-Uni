85379272c56f6944d4ed47753a524b5c22153c16
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:12 +0330

    Create check-if-a-character-is-a-vowel.c

---
+++ check-if-a-character-is-a-vowel.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main (){
+char ch;
+printf ("Enter a letter : ");
+ch=getch ();
+switch (ch){
+case 'A':
+case 'E':
+case 'I':
+case 'O':
+case 'U':
+case 'Y':
+case 'a':
+case 'e':
+case 'i':
+case 'o':
+case 'u':
+case 'y': printf("vowel\n");
+    break;
+default: printf("consonant\n",ch);
+}
+getch();
+return(0);
+}
