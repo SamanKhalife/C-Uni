a49e9a626ccc24fa9eedadd5295fb30e95707584
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:09 +0330

    Create calculate-y-based-on-x.c

---
+++ calculate-y-based-on-x.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main (){
+    float x,y;
+    printf("\n x = ");
+    scanf("%f",&x);
+    y=(x>20) ? -4*x*x+3 : 3*x-7;
+    printf("y=%-7.1f\n",y);
+    getchar();
+    return(0);
+}
