cd53b5068f317a18a068576c7e59a0585dc197a1
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:06 +0330

    Create calculate-the-factorial-of-a-number.c

---
+++ calculate-the-factorial-of-a-number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int i,n;
+long int f=1;
+
+printf("\nEnter n: ");
+scanf("%d",&n);
+
+for (i=1;i <= n;i++)
+    f *= i;
+
+printf("%d != %ld",n,f);
+getch ();
+return(0);
+}
