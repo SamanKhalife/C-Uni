9cf79e7cb43985d1aa0f4b0d2af08c0bbfa8eb94
Saman Khalife <saman.kh382@gmail.com>
2024-12-10 18:04:47 +0330

    Create display-a-triangle-of-numbers-in-Increasing-and-decreasing-order.c

---
+++ display-a-triangle-of-numbers-in-Increasing-and-decreasing-order.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+int n,i,j;
+printf("\nEnter n:");
+scanf("%d",&n);
+for (i=1;i <= n;i++)
+{
+printf(" ");
+for (j=1; j <= i; j++)
+printf("%3d",j);
+for (j=i-1;j>=1;j -- )
+printf("%3d",j);
+printf("\n");
+}
+getch ();
+return(0);
+}
