779f6cc82284d541abb095f775be332ff9373023
Saman Khalife <saman.kh382@gmail.com>
2024-12-15 01:14:54 +0330

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+  int n, i, j, count;
+  printf("\nEnter n : ");
+  scanf("%d", & n);
+  for (i = 2; i <= n; i++) {
+    count = 0;
+    for (j = 2; j <= i / 2; j++)
+      if (i % j == 0) {
+        count++;
+        break;
+      }
+
+    if (count == 0)
+      printf("%4d ", i);
+  }
+  getch();
+  return (0);
+}
