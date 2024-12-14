01ec466a1b3b11cbbd2ffa39a65eb9741a5df52b
Saman Khalife <saman.kh382@gmail.com>
2024-12-15 01:15:01 +0330

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,19 @@
+#include < stdio.h >
+  int main() {
+
+    int i, n;
+    printf("\nEnter n :");
+    scanf("%d", & n);
+    n = n % 7;
+    printf("\n sat sun mon tue wed thu fri\n");
+    for (i = 1; i <= n; i++)
+      printf("    ");
+
+    for (i = 1; i <= 31; i++) {
+      printf(" %2d ", i);
+      if ((i + n) % 7 == 0)
+        printf("\n");
+    }
+    getch();
+    return (0);
+  }
