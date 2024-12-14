c8dda66542d3c0fb716b3d7158d569239b67ce70
Saman Khalife <saman.kh382@gmail.com>
2024-12-15 01:14:09 +0330

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+  int i, A[6] = {
+    3,
+    7,
+    6,
+    27,
+    35,
+    12
+  };
+  for (i = 0; i < 6; i++):
+    printf("\nA[%d]=%d", i, A[i]);
+  getch();
+  return (0);
+}
